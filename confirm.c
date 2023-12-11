aec93e0b0783544a8a4f3959c6e237d67bae53df
hahearn73 <hahearn730@gmail.com>
2023-12-11 05:28:52 -0500

    exit

--- confirm.c
+++ confirm.c
@@ -5,7 +5,7 @@
 int main(int argc, char *argv[]) {
     if (argc < 2) {
         fprintf(stderr, "Usage: %s <command>\n", argv[0]);
-        return EXIT_FAILURE;
+        exit(0);
     }
 
     char response;
